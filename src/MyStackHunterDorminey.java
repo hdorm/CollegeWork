01152104498455dadb8c0d9280422e173b558c2d
Hunter Dorminey <hdormine@students.kennesaw.edu>
2022-07-01 22:11:40 -0400

    Initial commit

---
+++ src/MyStackHunterDorminey.java
@@ -0,0 +1,25 @@
+// Class:		    Data Structures W01
+// Term:		    Summer 2022
+// Name:		    Hunter Dorminey
+// Program Number:	Assignment 3
+// IDE: 		    IntelliJ, JDK 18.0.1
+
+import java.util.ArrayList;
+
+public class MyStackHunterDorminey<E> {
+
+    ArrayList<E> Assignment = new ArrayList<E>();
+
+    public static boolean isEmpty(){
+        return true;
+    }
+
+    public static int size(){
+        return 5;
+    }
+
+    public ArrayList<E> peek(ArrayList<E> Assignment){
+        return peek(Assignment);
+    }
+
+}
