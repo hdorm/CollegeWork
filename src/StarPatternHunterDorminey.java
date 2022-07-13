e54a9e4791c575ae967e8bf7d348320795fcfb78
Hunter Dorminey <hdormine@students.kennesaw.edu>
2022-07-12 23:51:48 -0400

    new tester

--- src/StarPatternHunterDorminey.java
+++
@@ -1,9 +0,0 @@
-// Class:		    Data Structures W01
-// Term:		    Summer 2022
-// Name:		    Hunter Dorminey
-// Program Number:	Assignment 1, Programming #3
-// IDE: 		    IntelliJ, JDK 18.0.1
-
-public class StarPatternHunterDorminey {
-
-}
